4a9c4c41da7bf4be94646c30782d2090909f2365
jabaringure@gmail.com <jabaringure@gmail.com>
2024-11-10 18:36:09 +0300

    Completes plantshop page

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
+    <li className="card">
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      {isSoldOut ? (
         <button>Out of Stock</button>
+      ) : (
+        <button onClick={() => setIsSoldOut(true)} className="primary">
+          In Stock
+        </button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchPlant, setsearchPlant] = useState("");
+
+  useEffect(()=> {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then((data) => setPlants(data));
+  }, []);
+
+  const theDisplayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+  );
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search searchPlant={searchPlant} setsearchPlant={setsearchPlant} />
+      <PlantList plants={theDisplayedPlants} />
     </main>
   );
 }
